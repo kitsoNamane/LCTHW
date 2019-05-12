2cfa48df4a80f41a93f043fd62416917c97586dc
kitsoNamane <kitsogideonnamane@gmail.com>
2019-05-12 23:26:42 +0200

    hello world in C

---
+++ ex1/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[]) {
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d kilometres away. \n", distance);
+
+    return 0;
+}
