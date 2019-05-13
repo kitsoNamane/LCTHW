9ba51c4e1b0ba591d01d27dd4a9e36bb5b62e896
kitsoNamane <kitsogideonnamane@gmail.com>
2019-05-13 12:20:34 +0200

    ex4 Makefile hotfix

---
+++ ex5/ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[]) {
+
+    int age = 24;
+    int height = 180;
+
+    // this is also a comment
+    printf("I am %d years old.\n", age);
+    printf("I am %d centimetres tall.\n", height);
+
+    return 0;
+}
