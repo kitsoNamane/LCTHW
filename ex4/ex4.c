14c6205bc18dd438ade0e4239a93e774db763757
kitsoNamane <kitsogideonnamane@gmail.com>
2019-05-13 12:00:12 +0200

    ex4 using the debugger

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -2,8 +2,15 @@
 
 /* This is a comment. */
 int main(int argc, char *argv[]) {
+    // ! HOW TO BREAK:
+    // * 1. int age;
+    // * 2. remove int age and compile
 
-    int age = 24;
+    // ! RESULTS:
+    // * 1. Compiles with warning: -Wuninitialized
+    // * 2. Error, does not compile
+
+    int age;
     int height = 180;
 
     // this is also a comment
